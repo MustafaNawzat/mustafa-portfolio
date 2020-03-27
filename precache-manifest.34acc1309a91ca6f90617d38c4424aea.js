701f243a7786d5b56dedc1733c2cd6656a553b75
Mustafa Nawzat <mustafanawzatt@gmail.com>
2020-03-27 16:05:01 +0300

    Updates

---
+++ precache-manifest.34acc1309a91ca6f90617d38c4424aea.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e44eb01525b304e423d04c72b7eff808",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d4db010200c2533ea72",
+    "url": "/static/css/2.18bc7be3.chunk.css"
+  },
+  {
+    "revision": "f4c7677c0d8c46c78825",
+    "url": "/static/css/main.3def0070.chunk.css"
+  },
+  {
+    "revision": "8d4db010200c2533ea72",
+    "url": "/static/js/2.de53fee9.chunk.js"
+  },
+  {
+    "revision": "fca8503ae7e1f8346dea3c73cc19c19f",
+    "url": "/static/js/2.de53fee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4c7677c0d8c46c78825",
+    "url": "/static/js/main.dba430aa.chunk.js"
+  },
+  {
+    "revision": "4c63620dcbac9f09e252",
+    "url": "/static/js/runtime-main.8c2719cc.js"
+  },
+  {
+    "revision": "e7594caf830b10ee0042ca4fd3d5f5a1",
+    "url": "/static/media/Mustafa_N_MERIE.e7594caf.pdf"
+  },
+  {
+    "revision": "ca844a15df6a14dcc840951a30678953",
+    "url": "/static/media/about.ca844a15.jpg"
+  },
+  {
+    "revision": "e18ac856bd091a8ffd42645a3bac3af4",
+    "url": "/static/media/project1.e18ac856.png"
+  },
+  {
+    "revision": "5db4d04e64c31872a194cfd299ee7a4c",
+    "url": "/static/media/project2.5db4d04e.png"
+  },
+  {
+    "revision": "5d087832ff815bb1f51371c6cb658698",
+    "url": "/static/media/project3.5d087832.png"
+  }
+]);
