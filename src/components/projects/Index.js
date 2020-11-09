06cc53bd62e2066db20ff6f44b583d756fafa2c3
MustafaNawzat <mustafanawzatt@gmail.com>
2020-11-09 22:21:44 +0300

    Projects Updated

--- src/components/projects/Index.js
+++ src/components/projects/Index.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react'
 import './style.css'
-import Pro1 from './img/project1.png'
-import Pro2 from './img/project2.png'
-import Pro3 from './img/project3.png'
-import Pro4 from './img/project4.png'
 // Bootstrap
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
@@ -24,6 +20,35 @@ export default class Projects extends Component {
           animation:  2s ${FadeInUpAnimation};
         `;
 
+
+        const projects = [
+            {
+                name: "Mentorship",
+                image: require("./img/project2.png"),
+                description: "Small website that allows you to find a Mentor/Mentee with the time that suits you and connect with them really easy.",
+                url: "https://osamaakb.github.io/mentorship"
+            },
+            {
+                name: "My Portfolio",
+                image: require("./img/project3.png"),
+                description: "Personal portfolio website template. The application is based on React and bootstrap 4+.",
+                url: "https://github.com/MustafaNawzat/mustafa-portfolio"
+            },
+            {
+                name: "ToDo App with ReactJS",
+                image: require("./img/project4.png"),
+                description: "This is an open source ToDo app using ReactJS with Bootstarp for styling and Context Api for state management. Beginners friendly.",
+                url: "https://mustafanawzat.github.io/todo-react/"
+            },
+            {
+                name: "Mustafaio",
+                image: require("./img/project1.png"),
+                description: "one-page portfolio website template. used the white space and the scroll effects to deliver an immersive experience.",
+                url: "https://mustafio.netlify.app/"
+            }
+        ]
+
+
         return (
             <>
                 <Container>
@@ -32,62 +57,23 @@ export default class Projects extends Component {
                         <p className="lead w-75 m-auto pb-4 section-p">Here is a list of the projects I have done by myself or with a team.</p>
                     </div>
                     <Row>
-                        {/* Project 1 */}
-                        <Col lg={6} className="mb-3">
-                            <FadeInUpDiv1s>
-                                <div className="hover hover-5 text-white rounded">
-                                    <img className="img-fluid" src={Pro1} alt="project"></img>
-                                    <div className="hover-overlay"></div>
-                                    <div className="hover-5-content">
-                                        <h4 className="hover-5-title text-uppercase text-center text-white text-bold">Mustafaio</h4>
-                                        <p className="lead porject-cption text-center text-white px-2">one-page portfolio website template. used the white space and the scroll effects to deliver an immersive experience.</p>
-                                        <a className="project-link text-center" href="https://mustafio.netlify.app/" target="_blank" rel='noreferrer noopener'><i className="fas fa-external-link-alt fa-2x text-white"></i></a>
-                                    </div>
-                                </div>
-                            </FadeInUpDiv1s>
-                        </Col>
-                        {/* Project 2 */}
-                        <Col lg={6} className="mb-3">
-                            <FadeInUpDiv2s>
-                                <div className="hover hover-5 text-white rounded">
-                                    <img className="img-fluid" src={Pro2} alt="project"></img>
-                                    <div className="hover-overlay"></div>
-                                    <div className="hover-5-content">
-                                        <h4 className="hover-5-title text-uppercase text-center text-white">Mentorship</h4>
-                                        <p className="lead porject-cption text-center text-white px-2">Small website that allows you to find a Mentor/Mentee with the time that suits you and connect with them really easy.</p>
-                                        <a className="project-link text-center" href="https://osamaakb.github.io/mentorship" target="_blank" rel='noreferrer noopener'><i className="fas fa-external-link-alt fa-2x text-white"></i></a>
-                                    </div>
-                                </div>
-                            </FadeInUpDiv2s>
-                        </Col>
-                        {/* Project 3 */}
-                        <Col lg={6} className="mb-3">
-                            <FadeInUpDiv2s>
-                                <div className="hover hover-5 text-white rounded">
-                                    <img className="img-fluid" src={Pro3} alt="project"></img>
-                                    <div className="hover-overlay"></div>
-                                    <div className="hover-5-content">
-                                        <h4 className="hover-5-title text-uppercase text-center text-white">My Portfolio</h4>
-                                        <p className="lead porject-cption text-center text-white px-2">Personal portfolio website template. The application is based on React and bootstrap 4+</p>
-                                        <a className="project-link text-center" href="https://github.com/MustafaNawzat/mustafa-portfolio" target="_blank" rel='noreferrer noopener'><i className="fas fa-external-link-alt fa-2x text-white"></i></a>
-                                    </div>
-                                </div>
-                            </FadeInUpDiv2s>
-                        </Col>
-                        {/* Project 4 */}
-                        <Col lg={6} className="mb-3">
-                            <FadeInUpDiv2s>
-                                <div className="hover hover-5 text-white rounded">
-                                    <img className="img-fluid" src={Pro4} alt="project"></img>
-                                    <div className="hover-overlay"></div>
-                                    <div className="hover-5-content">
-                                        <h4 className="hover-5-title text-uppercase text-center text-white">ToDo App with ReactJS</h4>
-                                        <p className="lead porject-cption text-center text-white px-2">This is an open source ToDo app using ReactJS with Bootstarp for styling and Context Api for state management. Beginners friendly.</p>
-                                        <a className="project-link text-center" href="https://mustafanawzat.github.io/ToDo-App-with-ReactJS-and-Context-Api/" target="_blank" rel='noreferrer noopener'><i className="fas fa-external-link-alt fa-2x text-white"></i></a>
-                                    </div>
-                                </div>
-                            </FadeInUpDiv2s>
-                        </Col>
+                        {
+                            projects.map(project => (
+                                <Col lg={6} className="mb-3">
+                                    <FadeInUpDiv1s>
+                                        <div className="hover hover-5 text-white rounded">
+                                            <img className="img-fluid" src={project.image} alt="project"></img>
+                                            <div className="hover-overlay"></div>
+                                            <div className="hover-5-content">
+                                                <h4 className="hover-5-title text-uppercase text-center text-white text-bold">{project.name}</h4>
+                                                <p className="lead porject-cption text-center text-white px-2">{project.description}</p>
+                                                <a className="project-link text-center" href={project.url} target="_blank" rel='noreferrer noopener'><i className="fas fa-external-link-alt fa-2x text-white"></i></a>
+                                            </div>
+                                        </div>
+                                    </FadeInUpDiv1s>
+                                </Col>
+                            ))
+                        }
                     </Row>
                 </Container>
             </>
